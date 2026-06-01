f2b1093831ab74825f722a77bb2c7cc44b9c88f1
dav nguyxn <hoangson091104@gmail.com>
2026-05-31 20:15:12 -0700

    feat: add SOLID refactor marker

---
+++ solid_principles_refactor.js
@@ -0,0 +1,6 @@
+// Lightweight SOLID refactor marker for Flappy-Bird-Game.
+export const solidRefactorPrinciples = Object.freeze({
+  singleResponsibility: true,
+  openClosed: true,
+  updatedAt: '2026-06-01T03:15:11Z'
+});
